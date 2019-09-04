7c57d0a730feb0639d202d415683c9c001de8698
Akshi Sinha <akshisinha1@gmail.com>
2019-09-04 16:49:06 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CurrentComponent } from './current/current.component';
+import { ForecastComponent } from './forecast/forecast.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: CurrentComponent
+  },
+  {
+    path: 'forecast',
+    pathMatch: 'full',
+    component: ForecastComponent
+  }
+  
+
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CurrentComponent,
+    ForecastComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
+  exports:[RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
